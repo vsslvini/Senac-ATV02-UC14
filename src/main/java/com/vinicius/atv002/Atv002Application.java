dc0fd4442fe147643f3ee23db66b6eb8a3687aae
Vinicius <vsslviniciussousa@gmail.com>
2024-05-12 20:43:59 -0300

    Configurações iniciais

---
+++ src/main/java/com/vinicius/atv002/Atv002Application.java
@@ -0,0 +1,13 @@
+package com.vinicius.atv002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atv002Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atv002Application.class, args);
+	}
+
+}
