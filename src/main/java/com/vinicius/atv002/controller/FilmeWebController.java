d1bcb5b44ef558d5ea4629850c625e7572be3758
Vinicius <vsslviniciussousa@gmail.com>
2024-05-20 16:06:34 -0300

    Adição do cookie para mudar tema das páginas

--- src/main/java/com/vinicius/atv002/controller/FilmeWebController.java
+++ src/main/java/com/vinicius/atv002/controller/FilmeWebController.java
@@ -6,13 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import com.vinicius.atv002.data.AvaliacaoEntity;
 import com.vinicius.atv002.data.FilmeEntity;
+import com.vinicius.atv002.data.Tema;
 import com.vinicius.atv002.services.AvaliacaoService;
 import com.vinicius.atv002.services.FilmeServices;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class FilmeWebController {
@@ -23,18 +30,59 @@ public class FilmeWebController {
 
     List<FilmeEntity> filmes = new ArrayList<FilmeEntity>();
     List<AvaliacaoEntity> avaliacoes = new ArrayList<AvaliacaoEntity>();
+    Tema tema = new Tema();
 
     @GetMapping("/")
-    public String getMethodName(Model model) {
+    public String getMethodName(@CookieValue(name = "tema", defaultValue = "") String cookie, @ModelAttribute Tema tema,
+            Model model, HttpServletResponse response) {
+
+        if (cookie.isEmpty()) {
+            // Define o tema padrão
+            String temaPadrao = "claro";
+            cookie = temaPadrao;
+
+            Cookie cookieTema = new Cookie("tema", temaPadrao);
+            cookieTema.setDomain("localhost");
+            cookieTema.setMaxAge(7 * 24 * 60 * 60);
+
+            response.addCookie(cookieTema);
+        }
+
         model.addAttribute("listaFilmes", filmeServices.ListarTodosFilmes());
+        model.addAttribute("tema", tema);
+        model.addAttribute("css", cookie);
+
         return "index";
     }
 
+    @PostMapping("/")
+    public ModelAndView criarCookie(@CookieValue(name = "tema", defaultValue = "claro") String cookie,
+            @ModelAttribute Tema tema, HttpServletResponse response) {
+        String temaEscolhido;
+
+        if ("claro".equals(cookie)) {
+            temaEscolhido = "escuro";
+        } else {
+            temaEscolhido = "claro";
+        }
+
+        Cookie cookieTema = new Cookie("tema", temaEscolhido);
+        cookieTema.setDomain("localhost");
+        cookieTema.setMaxAge(7 * 24 * 60 * 60);
+
+        response.addCookie(cookieTema);
+
+        return new ModelAndView("redirect:/");
+    }
+
     @GetMapping("/avaliacoes/{id}")
-    public String verAvaliacoes(@PathVariable(value = "id") Integer id, Model model) {
+    public String verAvaliacoes(@PathVariable(value = "id") Integer id, Model model,
+            @CookieValue(name = "tema", defaultValue = "claro") String cookie, Tema tema) {
         model.addAttribute("filme", filmeServices.getFilmeId(id));
         model.addAttribute("avaliacao", new AvaliacaoEntity());
         model.addAttribute("avaliacoes", avaliacaoService.listarAvaliacaoId(id));
+        model.addAttribute("css", cookie);
+        model.addAttribute("tema", tema);
         return "avaliar";
     }
 
@@ -50,8 +98,11 @@ public class FilmeWebController {
     }
 
     @GetMapping("/cadastroFilme")
-    public String exibirCadastro(Model model) {
+    public String exibirCadastro(Model model, @CookieValue(name = "tema", defaultValue = "claro") String cookie,
+            Tema tema) {
         model.addAttribute("filme", new FilmeEntity());
+        model.addAttribute("css", cookie);
+        model.addAttribute("tema", tema);
         return "cadastroFilme";
     }
 
@@ -64,7 +115,7 @@ public class FilmeWebController {
     }
 
     @GetMapping("/deletarFilme/{id}")
-    public String  deletarFilme(@PathVariable(value = "id") Integer id ){
+    public String deletarFilme(@PathVariable(value = "id") Integer id) {
         filmeServices.deletarFilme(id);
         return "redirect:/";
     }
---
+++ src/main/java/com/vinicius/atv002/data/Tema.java
@@ -0,0 +1,17 @@
+package com.vinicius.atv002.data;
+
+public class Tema {
+    public String tema;
+
+    public Tema() {
+       
+    }
+
+    public String getTema() {
+        return tema;
+    }
+
+    public void setTema(String tema) {
+        this.tema = tema;
+    }
+}
